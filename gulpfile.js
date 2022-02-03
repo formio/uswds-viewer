553b9c9fd4bb41eb911bca4c270e1d4387359f89
Travis Tidwell <travis@form.io>
2022-02-03 09:41:44 -0600

    Adding inline sources.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 var del = require('del');
 gulp.task('clean', () => del(['dist/*']));
 gulp.task('html', () => gulp.src('./src/index.html').pipe(gulp.dest('dist')));
@@ -9,7 +10,7 @@ gulp.task('seamless', () => gulp.src('./node_modules/seamless/build/**/*').pipe(
 gulp.task('font-awesome', () => gulp.src('./node_modules/font-awesome/css/font-awesome.css').pipe(gulp.dest('dist/lib/fontawesome')));
 gulp.task('uswds-styles', () => gulp.src('./node_modules/uswds/dist/css/uswds.min.css').pipe(gulp.dest('dist/lib/uswds')));
 gulp.task('uswds-img', () => gulp.src('./node_modules/uswds/dist/img/**/*').pipe(gulp.dest('dist/lib/img')));
-gulp.task('uswds-fonts', () => gulp.src(['./node_modules/uswds/dist/fonts/**/*', './node_modules/font-awesome/fonts/*']).pipe(gulp.dest('dist/lib/fonts')));
+gulp.task('uswds-fonts', () => gulp.src(['./node_modules/uswds/dist/fonts/**/*', './node_modules/font-awesome/fonts/*']).pipe(gulp.dest('dist/fonts')));
 gulp.task('custom', () => gulp.src('./node_modules/@formio/uswds/lib/css/styles.css').pipe(gulp.dest('dist/lib/uswds')));
 gulp.task('build', gulp.series('clean', gulp.parallel(
   'html',
@@ -23,6 +24,11 @@ gulp.task('build', gulp.series('clean', gulp.parallel(
   'uswds-img',
   'uswds-fonts',
 )));
+gulp.task('inlinesource', function () {
+  return gulp.src('./dist/*.html')
+      .pipe(plugins.inlineSource())
+      .pipe(gulp.dest('./dist'));
+});
 
 const s3 = require("gulp-s3");
 gulp.task('deploy', function () {
